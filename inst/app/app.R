c51dd1cd877fcd1ef68ca3b653bba6351336d108
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-01-25 16:57:41 +0100

    remove example, integrate import module into test app

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,35 +1,25 @@
 ui <- fluidPage(
-  # ======== Modules ========
-  # exampleModuleUI is defined in R/example-module.R
-  wellPanel(
-    h2("Modules example"),
-    exampleModuleUI("examplemodule1", "Click counter #1"),
-    exampleModuleUI("examplemodule2", "Click counter #2")
-  ),
-  # =========================
-
-  wellPanel(
-    h2("Sorting example"),
-    sliderInput("size", "Data size", min = 5, max = 20, value = 10),
-    div("Lexically sorted sequence:"),
-    verbatimTextOutput("sequence")
+  tagList(
+    navbarPage(
+      title = paste("Test App", packageVersion("IsoAppTools")),
+      theme = shinythemes::shinytheme("flatly"),
+      position = "fixed-top",
+      collapsible = TRUE,
+      id = "tab",
+      tabPanel(
+        title = "Import",
+        toolsPanelUI(id = "import_panel")
+      )
+    ),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    shinyjs::useShinyjs()
   )
 )
 
 server <- function(input, output, session) {
-  # ======== Modules ========
-  # exampleModuleServer is defined in R/example-module.R
-  exampleModuleServer("examplemodule1")
-  exampleModuleServer("examplemodule2")
-  # =========================
-
-  data <- reactive({
-    # lexical_sort from R/example.R
-    lexical_sort(seq_len(input$size))
-  })
-  output$sequence <- renderText({
-    paste(data(), collapse = " ")
-  })
+  toolsPanelServer(id = "import_panel")
 }
 
 shinyApp(ui, server)
