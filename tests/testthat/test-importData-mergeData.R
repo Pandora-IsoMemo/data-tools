0bc1c6de1da817bd7027c167a673416c86f444d9
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-01-26 09:17:48 +0100

    shift tests for queryData module, new tests for mergeData module

--- tests/testthat/test-importData-mergeData.R
+++ tests/testthat/test-importData-mergeData.R
@@ -206,11 +206,12 @@ test_that("matchColClasses/equalColClasses function", {
 
 })
 
-testthat::test_that("Test module queryDataServer", {
+
+testthat::test_that("Test module mergeDataServer", {
   testMergeList <-
     readRDS(testthat::test_path("test-importData-mergeData_data.rds"))
 
-  shiny::testServer(queryDataServer,
+  shiny::testServer(mergeDataServer,
                     args = list(mergeList = reactive(
                       list(
                         table1 = testMergeList[[1]]$dataImport,
@@ -219,83 +220,45 @@ testthat::test_that("Test module queryDataServer", {
                     )),
                     {
                       # Arrange
-                      print("test queryDataServer")
+                      print("test mergeDataServer")
                       # Act
-                      session$setInputs(sqlCommand = paste0("select t1.* from t1;"),
-                                        applyQuery = 0)
-                      testthat::expect_equal(dbListTables(inMemoryDB()),
-                                             c("t1", "t2"))
-                      testthat::expect_null(session$returned())
+                      session$setInputs(tableX = "table1",
+                                        tableY = "table2",
+                                        checkCommand = FALSE)
+
                       testthat::expect_equal(tableIds(),
-                                             c("t1", "t2"))
-                      testthat::expect_equal(inMemColumns()[["t1"]][1:3],
-                                             c("Human.Entry.ID", "Submitter.ID", "Context.ID"))
-                      testthat::expect_equal(inMemColumns()[["t2"]][1:3],
-                                             c("Entry.ID", "Submitter.ID", "Context.ID"))
-                      testthat::expect_true(is.character(output$inMemoryTables))
-                      testthat::expect_length(is.character(output$inMemoryTables), 1)
-                      testthat::expect_true(is.character(output$inMemoryColumns))
-                      testthat::expect_length(is.character(output$inMemoryColumns), 1)
-
-                      session$setInputs(
-                        sqlCommand = paste0("select t1.`Human.Entry.ID` from t1;"),
-                        applyQuery = 1
+                                             c(table1 = "table1", table2 = "table2"))
+                      testthat::expect_equal(tableXData() %>% nrow(), 3)
+                      testthat::expect_equal(tableXData() %>% ncol(), 51)
+                      testthat::expect_equal(tableYData() %>% nrow(), 3)
+                      testthat::expect_equal(tableYData() %>% ncol(), 81)
+                      testthat::expect_equal(
+                        extractCommon(colnames(tableXData()), colnames(tableYData()))[1:3],
+                        c("Submitter.ID", "Context.ID", "Individual.ID")
+                        )
+                      testthat::expect_equal(
+                        extractJoinString(c("Submitter.ID", "Context.ID", "Individual.ID"),
+                                          c("Submitter.ID", "Context.ID", "Individual.ID")),
+                        "c(\"Submitter.ID\"=\"Submitter.ID\", \"Context.ID\"=\"Context.ID\", \"Individual.ID\"=\"Individual.ID\")"
                       )
-
-                      testthat::expect_equal(session$returned(),
-                                             structure(
-                                               list(Human.Entry.ID = c(1, 2, 3)),
-                                               class = "data.frame",
-                                               row.names = c(NA,
-                                                             -3L)
-                                             ))
+                      # cannot test directly on joinedResult$data since it depends on the output
+                      # of a sub-module
+                      testthat::expect_equal(
+                        tableXData() %>%
+                            dplyr::left_join(
+                              tableYData(),
+                              by = c("Submitter.ID" = "Submitter.ID",
+                                     "Individual.ID" = "Individual.ID")) %>%
+                          nrow(),
+                        3
+                        )
+                      testthat::expect_equal(
+                        tableXData() %>%
+                          dplyr::left_join(
+                            tableYData(),
+                            by = c("Submitter.ID" = "Submitter.ID",
+                                   "Individual.ID" = "Individual.ID")) %>%
+                          ncol(),
+                        130)
                     })
-
-  testdb <- dbConnect(SQLite(), "file::memory:")
-
-  testTable1 <- testMergeList[[1]]$dataImport
-  testTable2 <- testMergeList[[2]]$dataImport
-
-  dbWriteTable(testdb, "table1", testTable1)
-  dbWriteTable(testdb, "table2", testTable2)
-
-  expect_equal(dbListTables(testdb), c("table1", "table2"))
-
-  testQuery <-
-    "SELECT t1.`Human.Entry.ID`, t1.`Age.Category`, t1.`Site.Name` FROM table1 AS t1 LEFT JOIN table2 as t2 ON t1.`Age.Category` = t2.`Age.Category` AND t1.`Site.Name` = t2.`Site.Name`;"
-
-  expect_equal(
-    RSQLite::dbGetQuery(testdb, testQuery),
-    structure(
-      list(
-        Human.Entry.ID = c(1, 2, 3),
-        Age.Category = c("Young Middle Adult",
-                         "Young Middle Adult", "Infant"),
-        Site.Name = c("Tertiveri", "Tertiveri",
-                      "Tertiveri")
-      ),
-      class = "data.frame",
-      row.names = c(NA, -3L)
-    )
-  )
-
-  testQueryFailure <-
-    "SELECT `Human.Entry.ID`, `Age.Category`, `Site.Name` FROM table1 LEFT JOIN table2 ON `Age.Category` = `Age.Category` AND `Site.Name` = `Site.Name`;"
-
-  expect_warning(tryCatch({
-    RSQLite::dbGetQuery(testdb, testQueryFailure)
-    #stop("test error")
-    #warning("test warning")
-  },
-  error = function(cond) {
-    warning(cond$message)
-    # Choose a return value in case of error
-    return(NULL)
-  },
-  warning = function(cond) {
-    warning(cond$message)
-    # Choose a return value in case of warning
-    return(NULL)
-  },
-  finally = NULL))
 })
