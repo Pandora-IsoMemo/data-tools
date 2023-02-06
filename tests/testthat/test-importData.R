42d6d056f50e9fc4e2e85441768b323388dbe88f
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-02-06 13:53:26 +0100

    test for batch import

--- tests/testthat/test-importData.R
+++ tests/testthat/test-importData.R
@@ -49,6 +49,82 @@ test_that("Test module importData", {
                  )
                )
              })
+
+  testServer(importDataServer,
+             {
+               # Arrange
+               print("test import of batch covariance")
+               # Act
+               session$setInputs(
+                 openPopup = TRUE,
+                 source = "file",
+                 type = "csv",
+                 rownames = FALSE,
+                 colSep = ",",
+                 decSep = ".",
+                 colnames = TRUE,
+                 includeSd = TRUE,
+                 file = structure(
+                   list(
+                     name = "batch_covariance.csv",
+                     size = 199L,
+                     type = "text/csv",
+                     datapath = testthat::test_path("batch_covariance.csv")
+                   ),
+                   class = "data.frame",
+                   row.names = c(NA,
+                                 -1L)
+                 ),
+                 accept = TRUE
+               )
+
+               # MUST be integrated into the module: ----
+               desiredOutput <- session$returned()[[1]] %>% as.matrix()
+               attr(desiredOutput, "includeSd") <- isTRUE(input$includeSd)
+               attr(desiredOutput, "includeRownames") <- isTRUE(input$rownames)
+
+               expect_equal(
+                 desiredOutput,
+                 structure(
+                   c(
+                     "Individual_1",
+                     "Individual_1",
+                     "Individual_2",
+                     "Individual_2",
+                     "Individual_3",
+                     "Individual_3",
+                     "Individual_4",
+                     "Individual_4",
+                     "Individual_5",
+                     "Individual_5",
+                     "1.0",
+                     "0.0",
+                     "1.0",
+                     "0.5",
+                     "1.0",
+                     "0.0",
+                     "1.0",
+                     "0.2",
+                     "1.0",
+                     "0.0",
+                     "0.0",
+                     "1.0",
+                     "0.0",
+                     "1.0",
+                     "0.3",
+                     "1.0",
+                     "0.0",
+                     "1.0",
+                     "0.0",
+                     "1.0"
+                   ),
+                   dim = c(10L, 3L),
+                   dimnames = list(NULL, c("target", "Carbon", "Nitrogen")),
+                   includeSd = TRUE,
+                   includeRownames = FALSE
+                 )
+               )
+             })
 })
 
 test_that("cutAllLongStrings function", {
