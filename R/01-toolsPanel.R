4bb638053f25ee4b8897ce5736557095e8089e0a
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-03-27 23:16:28 +0200

    change example data

--- R/01-toolsPanel.R
+++ R/01-toolsPanel.R
@@ -114,7 +114,30 @@ toolsLoadUI <- function(id) {
 toolsLoadServer <- function(id) {
   moduleServer(id,
                function(input, output, session) {
-                 testData <- reactiveVal(mtcars)
+                 testData <- reactiveVal(structure(
+                   list(
+                     mpg = c(21, 21, 22.8, 21.4, 18.7, 18.1),
+                     cyl = c(6, 6, 4, 6, 8, 6),
+                     disp = c(160, 160, 108, 258, 360, 225),
+                     hp = c(110, 110, 93, 110, 175, 105),
+                     drat = c(3.9, 3.9, 3.85, 3.08, 3.15, 2.76),
+                     wt = c(2.62, 2.875, 2.32, 3.215, 3.44, 3.46),
+                     qsec = c(16.46, 17.02, 18.61, 19.44, 17.02, 20.22),
+                     vs = c(0, 0, 1, 1, 0, 1),
+                     am = c(1, 1, 1, 0, 0, 0),
+                     gear = c(4, 4, 4, 3, 3, 3),
+                     carb = c(4, 4, 1, 1, 2, 1)
+                   ),
+                   row.names = c(
+                     "Mazda RX4",
+                     "Mazda RX4 Wag",
+                     "Datsun 710",
+                     "Hornet 4 Drive",
+                     "Hornet Sportabout",
+                     "Valiant"
+                   ),
+                   class = "data.frame"
+                 ))
 
                  downloadModelServer(
                    "download",
