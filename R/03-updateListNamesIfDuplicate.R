5f7f14b8e5809405eefb4f23899481bd55a0feea
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-10 17:08:23 +0200

    add function to update list entries called "name"

--- R/03-updateListNamesIfDuplicate.R
+++ R/03-updateListNamesIfDuplicate.R
@@ -1,4 +1,4 @@
-#' Update List Names if Conflicts
+#' Update List Names if Duplicate
 #'
 #' Updates the names of a list (newList), if the names already exist in another list (oldList).
 #' This is useful if two lists should be concatenated and duplicated names must be prevented.
@@ -11,25 +11,74 @@
 #'
 #' @export
 updateListNamesIfDuplicate <- function(newList, oldList, listType = "List", isShiny = TRUE) {
-  # check for name conflicts
-  if (all(!(names(newList) %in% names(oldList)))) return(newList)
+  if (is.null(oldList) || length(oldList) == 0) return(newList)
 
-  nameExists <- which(names(newList) %in% names(oldList))
-
-  if (isShiny) {
-    shinyjs::info(sprintf(
-      "Duplicated names found. '%s' name(s) \n %s \n already exist and will be replaced.",
-      listType,
-      paste(names(newList)[nameExists], collapse = ", ")
-    ))
-  }
-
-  # rename duplicated list names
   newNames <- names(newList)
-  while (any(newNames %in% names(oldList))) {
-    nameExists <- which(newNames %in% names(oldList))
-    newNames[nameExists] <- lapply(newNames[nameExists], incIndexOfName)
-    names(newList) <- newNames
+  oldNames <- names(oldList)
+
+  # check for duplicated names
+  nameExists <- which(newNames %in% oldNames)
+  if (length(nameExists) == 0) return(newList)
+
+  if (isShiny) infoDuplicates(listType, duplicates = paste(newNames[nameExists], collapse = ", "))
+
+  newList %>%
+    renameDuplicates(newNames,
+                     oldNames,
+                     assignNameFun = function(newList, newNames) {
+                       names(newList) <- newNames
+                       newList
+                     })
+}
+
+#' Update Names Entries if Duplicate
+#'
+#' Updates the entries called "name" of a list (newList), if the names already exist in another list
+#' (oldList).
+#' This is useful if two lists should be concatenated and duplicated names must be prevented.
+#'
+#' @param newList (list) The names of this list should be updated
+#' @param oldList (list) The names of this list should be kept
+#' @param listType (character) type of list, will be used in the notification if names had been
+#' changed.
+#' @param isShiny (logical) FALSE if not used in a shiny app, e.g. in tests.
+#'
+#' @export
+updateNameEntryIfDuplicate <- function(newList, oldList, listType = "List", isShiny = TRUE) {
+  if (is.null(oldList) || length(oldList) == 0) return(newList)
+
+  newNames <- sapply(newList, "[[", "name")
+  oldNames <- sapply(oldList, "[[", "name")
+
+  # check for duplicated names
+  nameExists <- which(newNames %in% oldNames)
+  if (length(nameExists) == 0) return(newList)
+
+  if (isShiny) infoDuplicates(listType, duplicates = paste(newNames[nameExists], collapse = ", "))
+
+  newList %>%
+    renameDuplicates(newNames,
+                     oldNames,
+                     assignNameFun = function(newList, newNames) {
+                       lapply(seq_along(newList), function(i) {
+                         newList[[i]][["name"]] <- newNames[i]
+                         newList[[i]]
+                       })
+                     })
+}
+
+infoDuplicates <- function(listType, duplicates) {
+  shinyjs::info(sprintf(
+    "Duplicated names found. '%s' name(s): \n %s \n already exist and will be replaced.",
+    listType, duplicates))
+}
+
+renameDuplicates <- function(newList, newNames, oldNames, assignNameFun) {
+  # rename duplicated list names
+  while (any(newNames %in% oldNames)) {
+    nameExists <- which(newNames %in% oldNames)
+    newNames[nameExists] <- sapply(newNames[nameExists], incIndexOfName)
+    newList <- assignNameFun(newList, newNames)
   }
 
   return(newList)
--- tests/testthat/test-updateListNamesIfDuplicate.R
+++ tests/testthat/test-updateListNamesIfDuplicate.R
@@ -1,16 +1,34 @@
 testthat::test_that("function updateListNamesIfDuplicate", {
   expect_equal(updateListNamesIfDuplicate(newList = list(c = 1, d = 2),
-                                         oldList = list(a = 7, b = 3),
-                                         isShiny = FALSE),
+                                          oldList = list(a = 7, b = 3),
+                                          isShiny = FALSE),
                list(`c` = 1, d = 2))
   expect_equal(updateListNamesIfDuplicate(newList = list(a = 1, d = 2),
-                                         oldList = list(a = 7, b = 3),
-                                         isShiny = FALSE),
+                                          oldList = list(a = 7, b = 3),
+                                          isShiny = FALSE),
                list(`a(1)` = 1, d = 2))
   expect_equal(updateListNamesIfDuplicate(newList = list(a = 1, d = 2),
-                                         oldList = list(a = 6, `a(1)` = 7, b = 3),
-                                         isShiny = FALSE),
+                                          oldList = list(a = 6, `a(1)` = 7, b = 3),
+                                          isShiny = FALSE),
                list(`a(2)` = 1, d = 2))
+})
+
+testthat::test_that("function updateNameEntryIfDuplicate", {
+  expect_equal(updateNameEntryIfDuplicate(
+    newList = list(list(x = 1, name = "c"), list(x = 2, name = "d")),
+    oldList = list(list(x = 7, name = "a"), list(x = 3, name = "b")),
+    isShiny = FALSE),
+    list(list(x = 1, name = "c"), list(x = 2, name = "d")))
+  expect_equal(updateNameEntryIfDuplicate(
+    newList = list(list(x = 1, name = "a"), list(x = 2, name = "d")),
+    oldList = list(list(x = 7, name = "a"), list(x = 3, name = "b")),
+    isShiny = FALSE),
+    list(list(x = 1, name = "a(1)"), list(x = 2, name = "d")))
+  expect_equal(updateNameEntryIfDuplicate(
+    newList = list(list(x = 1, name = "a"), list(x = 2, name = "d")),
+    oldList = list(list(x = 6, name = "a"), list(x = 7, name = "a(1)"), list(x = 3, name = "b")),
+    isShiny = FALSE),
+    list(list(x = 1, name = "a(2)"), list(x = 2, name = "d")))
 })
 
 testthat::test_that("function incIndexOfName", {
