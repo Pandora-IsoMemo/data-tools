929de883c78b0a3bd1dc10298bcf3487d7dd2c36
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-06-12 09:26:18 +0200

    remove test "expect_no_warning" for now

--- tests/testthat/test-isomemoAPI.R
+++ tests/testthat/test-isomemoAPI.R
@@ -9,14 +9,15 @@ test_that("Test getDatabaseList()", {
   expect_true(length(getDatabaseList()) > 0)
   expect_is(getDatabaseList(), "character")
   expect_equal(getDatabaseList(), getDatabaseList(mappingId = "IsoMemo"))
-  expect_no_warning(getDatabaseList(mappingId = "doNotExist"))
+  # function not yet available for version in docker
+  #expect_no_warning(getDatabaseList(mappingId = "doNotExist"))
 })
 
 test_that("Test getMappingTable()", {
   expect_true(length(getMappingTable()) > 0)
   expect_is(getMappingTable(), "data.frame")
   expect_equal(getMappingTable(), getMappingTable(mappingId = "IsoMemo"))
-  expect_no_warning(getMappingTable(mappingId = "doNotExist"))
+  #expect_no_warning(getMappingTable(mappingId = "doNotExist"))
   expect_true(all(colnames(getMappingTable()) %in% c("shiny", "fieldType", "category", "mappingId")))
 })
 
@@ -24,7 +25,7 @@ test_that("Test getRemoteData()", {
   expect_true(length(getRemoteData(db = "LiVES")) > 0)
   expect_is(getRemoteData(db = "LiVES"), "data.frame")
   expect_equal(getRemoteData(db = "LiVES"), getRemoteData(mappingId = "IsoMemo", db = "LiVES"))
-  expect_no_warning(getRemoteData(mappingId = "doNotExist", db = "LiVES"))
+  #expect_no_warning(getRemoteData(mappingId = "doNotExist", db = "LiVES"))
   expect_equal(unique(getRemoteData(db = "LiVES")$source),
                structure(1L, levels = "LiVES", class = "factor"))
 })
