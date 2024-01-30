02c628f6630e0a174fdcf5a0138d3d4e0c7878d0
Jan A <jan.abel@inwt-statistics.de>
2024-01-30 10:43:11 +0100

    define global vars - api_key & openai url

--- R/00-config.R
+++ R/00-config.R
@@ -2,3 +2,16 @@ config <- function() {
   config_path <- system.file("config.yaml", package = "DataTools")
   yaml.load_file(config_path)
 }
+
+#' globals
+#' @description
+#' Contains the package's global variables
+pkg.env = new.env()
+pkg.env$api_key = NULL
+
+
+#' Contains the package's base URLs
+#'
+#' @description
+#' These are the base URLs for the `rgpt3` package. Do not change these!
+url.completions = "https://api.openai.com/v1/completions"
