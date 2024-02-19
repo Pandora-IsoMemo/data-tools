e52cfd5f0d3674b632ef9621e331d43ea31d6fac
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-02-19 21:14:21 +0100

    use loadImport in dataLinkUpload

--- R/02-importData-linkToData.R
+++ R/02-importData-linkToData.R
@@ -15,10 +15,10 @@ observeDownloadDataLink <- function(id, input, output, session) {
       ]]
 
       list(
-        dataSource = sourceInputs,
-        dataPreparation = NULL,
-        dataMerging = NULL,
-        dataQuery = NULL
+        source = sourceInputs,
+        preparation = NULL,
+        merging = NULL,
+        query = NULL
       )
   })
 
@@ -38,14 +38,13 @@ observeDownloadDataLink <- function(id, input, output, session) {
   )
 }
 
-
 #' Download a link to import data
 #'
 #' @param id module id
 #' @param input input object from server function
 #' @param output output object from server function
 #' @param session session from server function
-observeUploadDataLink <- function(id, input, output, session) {
+observeUploadDataLink <- function(id, input, output, session, importParams) {
   dataLinkUpload <- reactiveValues(
     loaded = 0,
     import = list()
@@ -55,6 +54,7 @@ observeUploadDataLink <- function(id, input, output, session) {
   observe({
     req(input[["dataSelector-fileSource-dataOrLink"]] == "dataLink")
 
+    # check if upload is a json file
     file <- input[["dataSelector-fileSource-file"]]
     fileType <- file$datapath %>%
       basename() %>%
@@ -62,18 +62,21 @@ observeUploadDataLink <- function(id, input, output, session) {
 
     req(fileType == "json")
 
+    # read json
     dataLinkUpload$import <- jsonlite::read_json(file$datapath)
 
     req(length(dataLinkUpload$import) > 0,
-        !is.null(dataLinkUpload$import[["dataSource"]]))
-    loadedInputs <- dataLinkUpload$import[["dataSource"]]
+        length(dataLinkUpload$import[["source"]]) > 0)
+
+    # loadedSourceInputs will be a list later
+    loadedSourceInputs <- dataLinkUpload$import[["source"]]
 
     ## update inputs ----
-    inputIDs <- names(loadedInputs)
+    inputIDs <- names(loadedSourceInputs)
     inputIDs <- inputIDs[inputIDs %in% names(input)]
 
     for (i in 1:length(inputIDs)) {
-      session$sendInputMessage(inputIDs[i],  list(value = loadedInputs[[inputIDs[i]]]))
+      session$sendInputMessage(inputIDs[i],  list(value = loadedSourceInputs[[inputIDs[i]]]))
     }
 
     dataLinkUpload$loaded <- dataLinkUpload$loaded + 1
@@ -83,19 +86,37 @@ observeUploadDataLink <- function(id, input, output, session) {
   # observe if a link was imported and load the data from the "link"
   observe({
     req(dataLinkUpload$loaded > 0)
-    #browser()
-    # this part is not working
-    # session$sendInputMessage("dataSelector-fileSource-source",
-    #                          list(value = dataLinkUpload$import[["dataSource"]][["dataSelector-fileSource-source"]]))
+
+    # loadedSourceInputs must be a list later
+    loadedSourceInputs <- dataLinkUpload$import[["source"]]
 
     # load data from ckan
     ## apply load button ckan if ckan:
-    if (dataLinkUpload$import[["dataSource"]][["dataSelector-fileSource-source"]] == "ckan") {
-      # session$sendInputMessage("dataSelector-fileSource-resourceLoad-loadCKAN",
-      #                          list(value = input[["dataSelector-fileSource-resourceLoad-loadCKAN"]] + 1))
-      # we need to execute the import directly! ####
+    if (loadedSourceInputs[["dataSelector-fileSource-source"]] == "ckan") {
+      browser()
 
-      # load function here!!! ----
+      # !!!! use loadedSourceInputs for
+      # - inputFileSource,
+      # - source -> getDataSource()
+
+      withProgress(
+        value = 0.75,
+        message = 'Importing ...', {
+          values <- loadImport(
+            importType = importType,
+            filename = dataSource$filename,
+            expectedFileInZip = expectedFileInZip,
+            params = list(values = values,
+                          dataSource = dataSource,
+                          inputFileSource = reactiveValuesToList(
+                            input)[grepl("fileSource", names(input))],
+                          customNames = customNames,
+                          subFolder = subFolder,
+                          rPackageName = rPackageName,
+                          onlySettings = onlySettings,
+                          fileExtension = fileExtension)
+          )
+        })
     }
   }) %>%
     bindEvent(dataLinkUpload$loaded)
