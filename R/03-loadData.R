800124dcc66ab641eb10959457d913514fa7a2a2
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-08-13 09:40:49 +0200

    add reset

--- R/03-loadData.R
+++ R/03-loadData.R
@@ -20,6 +20,7 @@ loadDataWrapper <- function(values,
                             withRownames,
                             withColnames,
                             sheetId) {
+  values <- values %>% resetValues()
   if (is.null(filepath)) return(values)
 
   df <- tryCatch(
